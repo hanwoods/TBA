aafaa9f8a9cd7cf4ec2d7d47ee16e18359453366
Justin Lee <justinlee0022@gmail.com>
2016-02-28 00:20:33 -0800

    got rid of login button on main page

--- client/templates/auth-signin.js
+++ client/templates/auth-signin.js
@@ -44,21 +44,3 @@ Template.signin.events({
     });
   }
 });
-
-Template.login.events({
-  'click #facebook-login': function(event) {
-    Meteor.loginWithFacebook({}, function(err){
-      if (err) {
-        throw new Meteor.Error("Facebook login failed");
-      }
-    });
-  },
-
-  'click #logout': function(event) {
-    Meteor.logout(function(err){
-      if (err) {
-        throw new Meteor.Error("Logout failed");
-      }
-    })
-  }
-});
